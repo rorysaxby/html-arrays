eb75f61bd7c4b571da86aa3f0759f52241220e82
rorysaxby <rory.saxby@gmail.com>
2018-05-02 21:15:02 +0100

    inspect array and check item type

--- script.js
+++ script.js
@@ -5,8 +5,30 @@
     ****************************/
 
     var htmlArray = function(obj){
+        this.array = obj.array;
+        this.callback = obj.callback;
+        
+        this.html = '<' + this.array[0] + '>';
+        this.openElements = [this.array[0]];
 
+        this.inspectArray(this.array);
     };
+
+    htmlArray.prototype.inspectArray = function(array){
+        if(this.array.length > 1){
+            for(var i = 1; i < this.array.length; i++){
+                this.checkType(this.array[i]);
+            };
+        } else{
+            this.addCloseElement();
+        };
+    };
+
+    htmlArray.prototype.checkType = function(item){
+        var type = typeof item;
+        
+    };
+
 
     /**************************
     - HTML Array demo 
@@ -22,7 +44,7 @@
     };
 
     htmlArrayDemo.displayString = function(){
-        
+
     };
 
     htmlArrayDemo.init = function(){
