3e75f5e8a4e534ce5f665fa570d295d144b6769a
rorysaxby <rory.saxby@gmail.com>
2018-05-02 21:26:55 +0100

    add open element, add string, add close element functions

--- script.js
+++ script.js
@@ -12,12 +12,18 @@
         this.openElements = [this.array[0]];
 
         this.inspectArray(this.array);
+        
+        console.log(this.html);
     };
 
     htmlArray.prototype.inspectArray = function(array){
         if(this.array.length > 1){
             for(var i = 1; i < this.array.length; i++){
                 this.checkType(this.array[i]);
+
+                if(i + 1 == array.length){
+                    this.addCloseElement();
+                };
             };
         } else{
             this.addCloseElement();
@@ -26,7 +32,25 @@
 
     htmlArray.prototype.checkType = function(item){
         var type = typeof item;
-        
+
+        switch(type){
+            case 'string':
+                this.addString(item);
+            break;
+            case 'object':
+                this.addOpenElement(item);
+            break;
+        };
+    };
+
+    htmlArray.prototype.addOpenElement = function(array){
+        this.html = this.html + '<' + array[0] + '>';
+        this.openElements.unshift(array[0]);
+    };
+
+    htmlArray.prototype.addCloseElement = function(){
+        this.html = this.html + '</' + this.openElements[0] + '>';
+        this.openElements.splice(0, 1);
     };
 
 
@@ -38,7 +62,7 @@
 
     htmlArrayDemo.getHtmlString = function(){
         var demoString = new htmlArray({
-            array:['div'],
+            array:['div', 'test', ['h1', 'header']],
             callbackFn: htmlArrayDemo.displayString
         });
     };
@@ -49,6 +73,7 @@
 
     htmlArrayDemo.init = function(){
         htmlArrayDemo.target = document.getElementsByClassName('html-output');
+        htmlArrayDemo.getHtmlString();
     }();
 
 })();
